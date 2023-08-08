8039697f88beb83db6531b92eae9e7e40475f887
kyeul611 <gyul611@likelion.org>
2023-08-08 12:03:36 +0900

    이미 수집한 리뷰를 다시 수집하지 않도록 코드 추가

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 리눅스에서 테스트를 진행해야함.
 리뷰 데이터의 경우 1000페이지 이후 데이터가 없음.
 '''
-
 from crawler import CrawlingItem
 
 import pandas as pd
@@ -28,8 +27,6 @@ if __name__=='__main__':
     if not os.path.exists('logs'):
         os.mkdir('logs')
 
-
-
     query_url = query.replace('_', '%20') # url용 띄어쓰기 문자열 치환
 
     cItem = CrawlingItem
@@ -44,29 +41,31 @@ if __name__=='__main__':
         df = pd.DataFrame()
         exists_items = []
 
+    # 리뷰 데이터가 있으면 목록을 가져온다.
+    if os.path.exists(f'reviews/{query}'):
+        exists_review = [file.split('.')[0] for file in os.listdir(f'reviews/{query}')]
+
     # 정보와 리뷰 수집
     for i, url in enumerate(product_urls):
-        # 아이템 정보 수집
         print(f"[{i+1}/{len(product_urls)}]", end="")
-        item_info, flag = cItem.getProdInfo(url, query)
-
-        if flag == False: # 리뷰데이터 수집을 못했다면
-            continue
-        # 만약 이미 수집한 데이터라면 리뷰 수집은 하지 않고 건너 뜀
-        if int(item_info.iloc[-1]['상품번호']) in exists_items:
+        
+        # 아이템 정보 수집
+        item_info = cItem.getProdInfo(url, query)
+        if int(item_info.iloc[-1]['상품번호']) not in exists_items:
+            # 수집한 데이터를 저장함.
+            df = pd.concat([df, item_info], ignore_index=True) # 아이템 정보를 수집 후 기존 정보와 병합
+            df.fillna(method='ffill', inplace=True) 
+            df.drop_duplicates(inplace=True)
+            df.to_csv(f'itemData/{query}.csv', encoding='utf-8', mode='w') # 데이터를 덮어씀. 지속적으로 IO가 일어나기 때문에 성능에 영향을 끼칠 수 있지만, column이 통일되지 않은 상황에서 데이터를 지속적으로 저장하기 위한 차선책.
+        
+        # 이미 수집한 리뷰라면 건너 뛴다. 
+        if int(item_info.iloc[-1]['상품번호']) in exists_review:
             print(">> 이미 있는 데이터이므로 다음으로 넘어갑니다. ")
             continue
-        
-        # 수집한 데이터를 저장함.
-        df = pd.concat([df, item_info], ignore_index=True) # 아이템 정보를 수집 후 기존 정보와 병합
-        df.fillna(method='ffill', inplace=True) 
-        df.to_csv(f'itemData/{query}.csv', encoding='utf-8', mode='w') # 데이터를 덮어씀. 지속적으로 IO가 일어나기 때문에 성능에 영향을 끼칠 수 있지만, column이 통일되지 않은 상황에서 데이터를 지속적으로 저장하기 위한 차선책.
-        
+
         # 리뷰 데이터 수집
         pNum = df.iloc[-1]['상품번호']
         nReview = int(df.iloc[-1]['nReview'])
         cItem.getProdReview(pNum, query, nReview) # 리뷰 수집
 
-    
-    
     print("수집 완료!")
