bf1506f9a4e75fdb2037ee3fc2b31b9d64188244
Park Kyeul <gyul611@likelion.org>
2023-08-11 23:37:23 +0900

    자잘한 수정

--- crawler.py
+++ crawler.py
@@ -20,7 +20,7 @@ import itertools
 import traceback
 
 options = Options()
-options.add_argument('--headless')
+# options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-usb-devices')
