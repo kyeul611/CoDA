94b4668d2a0a56e79e87e52830458473c676963e
Kim, Jaeseong <06zzkimzz06@gmail.com>
2023-08-22 19:20:06 +0900

    테스트

--- test.py
+++ test.py
@@ -4,4 +4,9 @@ import pandas as pd
 import os
 import argparse
 
-CrawlingBlogItem.getBlogUrls("커피", 10)
+#CrawlingBlogItem.getBlogUrls("커피", 10)
+
+url = "https://blog.naver.com/ko4791917/223188365892"
+
+
+CrawlingBlogItem.getBlogReviews(url, "")
