428b8f63d027a8e6acbb9bf30e53ddc2b3d210a2
Tiabet <kkksk1157@naver.com>
2023-08-25 17:00:27 +0900

    텍스트데이터 전처리 파일입니다.

--- DA/text_preprocessing_pipeline.py
+++ DA/text_preprocessing_pipeline.py
@@ -1,5 +1,5 @@
-#사용법 : python text_preprocessing_pipeline.py input.tsv
-#결과가 input 파일 이름_preprocessed.tsv 형식으로 저장됩니다.
+#사용법 : python text_preprocessing_pipeline.py 폴더명
+#결과가 폴더명_processed 폴더에 파일명_preprocessed.tsv 형식으로 저장됩니다.
 
 import unicodedata
 import pandas as pd
@@ -27,6 +27,8 @@ def apply_regex(text):
     text = re.sub(r'[^ 가-힣a-zA-Z\(\):]','',text)
     text = re.sub(r'[a-zA-Z]{1,2}', '', text)
     text = re.sub(r':\s?[\)D]|:\s?\(', '', text)
+    #text = re.sub(r'\b(\w+)(?: \1\b)+', r'\1', text)
+    text = re.sub(r'\s+', ' ', text)
     return text
 
 class NormalizeUnicodeTransformer(BaseEstimator, TransformerMixin):
@@ -53,9 +55,10 @@ class ApplyRegexTransformer(BaseEstimator, TransformerMixin):
         print("Applying Regex")
         return X.apply(apply_regex)
 
-def preprocess_file(input_filename, output_filename):
+def preprocess_file(input_filename, output_folder):
     df = pd.read_csv(input_filename, sep='\t')
-    print("{}의 리뷰를 전처리 중입니다...".format(input_filename))
+    input_folder, input_file = input_filename.split('\\')[-2], input_filename.split('\\')[-1]
+    print("{}의 리뷰를 전처리 중입니다...".format(input_file))
     print("리뷰 개수 : {}".format(len(df['review'])))
     
     preprocessing_pipeline = Pipeline([
@@ -66,17 +69,29 @@ def preprocess_file(input_filename, output_filename):
 
     df_preprocessed = preprocessing_pipeline.fit_transform(df['review'])
     df['review'] = df_preprocessed
-    df.to_csv(output_filename, sep='\t', index=False)
-    print("저장이 완료되었습니다.")
+    
+    base_filename, extension = os.path.splitext(input_file)
+    output_filename = base_filename + '_preprocessed' + extension
+    output_file = os.path.join(output_folder, output_filename)
+    df.to_csv(output_file, sep='\t', index=False)
+    print("{} 저장이 완료되었습니다.".format(input_file))
+    print("")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="텍스트 데이터 전처리(띄어쓰기, 불필요언어 제거 등)")
-    parser.add_argument("input_file", help="전처리할 파일명을 입력하세요.")
+    parser.add_argument("input_folder", help="전처리할 파일들이 있는 폴더명을 입력하세요.")
     
     args = parser.parse_args()
     
-    input_filename = args.input_file
-    base_filename, extension = os.path.splitext(input_filename)
-    output_filename = base_filename + '_preprocessed' + extension
+    input_folder = args.input_folder
+    output_folder = input_folder+"_preprocessed"
     
-    preprocess_file(input_filename, output_filename)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    print("{} 폴더의 저장을 시작합니다.".format(input_folder))
+    print("")
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".tsv"):
+            input_filename = os.path.join(input_folder, filename)
+            preprocess_file(input_filename, output_folder)
